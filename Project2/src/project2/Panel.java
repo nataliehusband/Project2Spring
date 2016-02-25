a5eb04db3ad70b9e632bb02b821a7c589ceb0f70
nataliehusband <nataliehusband@10.21.11.60>
2016-02-24 21:01:50 -0700

    //added a JFrame and JOptionpane welcome

--- Project2/src/project2/Panel.java
+++ Project2/src/project2/Panel.java
@@ -1,9 +1,25 @@
 package project2;
 
+import java.awt.FlowLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Panel extends JFrame
+public class Panel extends JFrame //http://chortle.ccsu.edu/java5/notes/chap56/ch56_11.html
 {
 	JPanel newpanel; 
+	JLabel newlabel; 
+	
+	//constructor
+	Panel(String title)
+	{
+		super(title); 
+		setSize(600, 600); 
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null); 
+		
+		setLayout( new FlowLayout() ); 
+		newlabel = new JLabel("Welcome to Swing"); 
+		add(newlabel); 	
+	}
 }
--- Project2/src/project2/mainClass.java
+++ Project2/src/project2/mainClass.java
@@ -1,9 +1,14 @@
 package project2;
 
+import javax.swing.JOptionPane;
+
 public class mainClass 
-{
+{		
+	static Panel frame = new Panel("Hello"); 
+
 	public static void main(String[] args) 
 	{
-		
+		JOptionPane.showMessageDialog(null, "Welcome to this program!");
+		frame.setVisible(true); 
 	}
 }
