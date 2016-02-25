f74758a8c763b1f02df20e3ca2558123a43b8a37
Kyle <Kyle@10.21.16.172>
2016-02-25 00:59:55 -0700

    prints a 2d array with a catalog file

--- Project2/src/project2/Superclass.java
+++ Project2/src/project2/Superclass.java
@@ -8,7 +8,7 @@ public class Superclass
 	private static File file = new File("Catalog");
 	private static Scanner in;
 	private final int SUBJECTS = 5;
-	private int value;
+	private int[] value = {0};
 	private String[][] nameArray;
 	
 	public Superclass(){
@@ -17,18 +17,36 @@ public class Superclass
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		value = 1;
-		nameArray = new String[value][SUBJECTS];
+		String g;
 		while(in.hasNextLine()){
-			for(int i = 0;i<value;i++){
-				for(int j = 0;j<SUBJECTS;j++){
-					String q = in.nextLine();
-					q.trim();
-					nameArray[i][j] = q;
-					System.out.print(nameArray[i][j] + "  ");
-				}
-				System.out.println("");
-			}
+			value[0] = value[0]+1;
+			g=in.nextLine();
 		}
+		value[0] = value[0]/5;
+		nameArray = new String[value[0]][SUBJECTS];
+		try {
+			in = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		for(int i = 0;i<value[0];i++){
+			for(int j = 0;j<SUBJECTS;j++){
+				String q = in.nextLine();
+				q.trim();
+				nameArray[i][j] = q;
+				System.out.print(nameArray[i][j] + "  ");
+			}
+			System.out.println("");
+		}
+		
+	}
+	
+	public String[][] getArray(){
+		
+		return nameArray;
+	}
+	
+	public int getValue(){
+		return value[0];
 	}
 }
