930df46427204a66799d8bb0c9a344b605dfd85d
nataliehusband <nataliehusband@10.21.11.60>
2016-02-24 20:45:26 -0700

    //new classes, JFrame, etc.

---
+++ Project2/src/project2/Panel.java
@@ -0,0 +1,9 @@
+package project2;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Panel extends JFrame
+{
+	JPanel newpanel; 
+}
---
+++ Project2/src/project2/mainClass.java
@@ -0,0 +1,9 @@
+package project2;
+
+public class mainClass 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+}
