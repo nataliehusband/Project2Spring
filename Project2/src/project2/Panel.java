f566baa5e4fa99cec625d376420d6a3b32b96cef
nataliehusband <nataliehusband@10.21.11.60>
2016-02-24 21:18:29 -0700

    for kyle

--- Project2/src/project2/Panel.java
+++ Project2/src/project2/Panel.java
@@ -4,11 +4,13 @@ import java.awt.FlowLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class Panel extends JFrame //http://chortle.ccsu.edu/java5/notes/chap56/ch56_11.html
 {
 	JPanel newpanel; 
 	JLabel newlabel; 
+	JTextArea blah = new JTextArea(2, 15); 
 	
 	//constructor
 	Panel(String title)
@@ -17,9 +19,12 @@ public class Panel extends JFrame //http://chortle.ccsu.edu/java5/notes/chap56/c
 		setSize(600, 600); 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null); 
-		
+	
 		setLayout( new FlowLayout() ); 
 		newlabel = new JLabel("Welcome to Swing"); 
-		add(newlabel); 	
+		add(newlabel); 
+		blah.setLocation(25,25);
+		blah.setSize(550,550);
+		add(blah); 
 	}
 }
