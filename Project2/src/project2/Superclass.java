31fa1acee90116291c2c148623c7e0c0592d043f
nataliehusband <nataliehusband@10.21.11.60>
2016-02-25 18:33:23 -0700

    fixed superclass

--- Project2/src/project2/Superclass.java
+++ Project2/src/project2/Superclass.java
@@ -1,6 +1,53 @@
 package project2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Superclass 
 {
-
+	private static File file = new File("Catalog");
+	private static Scanner in;
+	private final int SUBJECTS = 5;
+	private int[] value = {0};
+	private String[][] nameArray;
+	
+	public Superclass(){
+		try {
+			in = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		String g;
+		while(in.hasNextLine()){
+			value[0] = value[0]+1;
+			g=in.nextLine();
+		}
+		value[0] = value[0]/5;
+		nameArray = new String[value[0]][SUBJECTS];
+		try {
+			in = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		for(int i = 0;i<value[0];i++){
+			for(int j = 0;j<SUBJECTS;j++){
+				String q = in.nextLine();
+				q.trim();
+				nameArray[i][j] = q;
+				System.out.print(nameArray[i][j] + "  ");
+			}
+			System.out.println("");
+		}
+		
+	}
+	
+	public String[][] getArray(){
+		
+		return nameArray;
+	}
+	
+	public int getValue(){
+		return value[0];
+	}
 }
