988c81d531800b8d51f274b90aba5e6a702223db
nataliehusband <nataliehusband@10.21.11.60>
2016-02-25 18:29:28 -0700

    changes to panel

--- Project2/src/project2/Panel.java
+++ Project2/src/project2/Panel.java
@@ -12,11 +12,13 @@ import javax.swing.JTextField;
 public class Panel extends JFrame //http://chortle.ccsu.edu/java5/notes/chap56/ch56_11.html
 {
 	JPanel newpanel; 
+	JLabel intro; 
 	JLabel productName; 
 	JLabel quantity; 
 	JLabel price; 
 	JLabel numericID; 
 	JLabel category; 
+	JLabel space; 
 	JTextField productAnswer = new JTextField(10); //http://www.wideskills.com/java-tutorial/java-jtextfield-class-example
 	JTextField quantityAns = new JTextField(10); 
 	JTextField priceAns = new JTextField(10); 
@@ -34,12 +36,15 @@ public class Panel extends JFrame //http://chortle.ccsu.edu/java5/notes/chap56/c
 		setLayout(new FlowLayout(FlowLayout.LEFT, 10, 25));
 		
 		//initializes all of the JLabels 
+		intro = new JLabel("                    Product Information Sheet:                ");
 		productName = new JLabel("Product Name:          "); 
 		quantity = new JLabel("Quantity of Item:      ");
 		price = new JLabel("Price of Item:            "); 
 		numericID = new JLabel("Numeric ID of item: "); 
 		category = new JLabel("Category:                 "); 
+		space = new JLabel("                            ");
 	
+		add(intro); 
 		add(productName);
 		productAnswer.setSize(100,20);
 		add(productAnswer);	
@@ -60,15 +65,17 @@ public class Panel extends JFrame //http://chortle.ccsu.edu/java5/notes/chap56/c
 		categoryAns.setSize(100, 20);
 		add(categoryAns); 
 		
+		add(space); 
 		finished.setLocation(200, 200);
 		add(finished); 
-		finished.addActionListener(null);
+		finished.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				setVisible(false); 
+			}
+		});
 
 		setVisible(true);
-	}
-	
-	public void ActionPerformed(ActionEvent e)
-	{
-		
 	}
 }
